143082e366cfdf8e5e1b0a3c8e7bbcc11497aaec
Jayant Sinha <jayant.sinha@hotmail.com>
2016-08-04 17:45:58 +0530

    Added face field in Card

--- card.go
+++ card.go
@@ -1,54 +1,63 @@
 package main
 
+import "strconv"
+
 //Faces
 const (
-  TWO int = iota
-  THREE
-  FOUR
-  FIVE
-  SIX
-  SEVEN
-  EIGHT
-  NINE
-  TEN
-  JACK
-  QUEEN
-  KING
-  ACE
+	TWO int = iota
+	THREE
+	FOUR
+	FIVE
+	SIX
+	SEVEN
+	EIGHT
+	NINE
+	TEN
+	JACK
+	QUEEN
+	KING
+	ACE
 )
 
 //Suits
-const(
-  SUIT_HEARTS = "H"
-  SUIT_SPADES= "S"
-  SUIT_DIAMONDS = "D"
-  SUIT_BLACKS = "B"
+const (
+	SUIT_HEARTS   = "H"
+	SUIT_SPADES   = "S"
+	SUIT_DIAMONDS = "D"
+	SUIT_BLACKS   = "B"
 )
 
 type Card struct {
-  suit string
-  value int
-  alternateValue int  //Same as value except for Aces
+	suit           string
+	face           string
+	value          int
+	alternateValue int //Same as value except for Aces
 }
 
 //Returns a new new card
 func NewCard(s string, val int) Card {
-  v, altVal := getCardValues(val)
-  card := Card {
-    suit:s,
-    value:v,
-    alternateValue:altVal,
-  }
-  return card
+	f, v, altVal := getCardValues(val)
+	card := Card{
+		suit:           s,
+		face:           f,
+		value:          v,
+		alternateValue: altVal,
+	}
+	return card
 }
 
 //Get card values
-func getCardValues(val int) (value, alternateValue int) {
-  switch val {
-  case JACK, QUEEN, KING:
-    return 10, 10
-  case ACE:
-    return 11, 1
-  }
-  return val+2, val+2
+func getCardValues(val int) (face string, value, alternateValue int) {
+	switch val {
+	case JACK:
+		return "J", 10, 10
+	case QUEEN:
+		return "Q", 10, 10
+	case KING:
+		return "K", 10, 10
+	case ACE:
+		return "A", 11, 1
+	}
+	f := strconv.Itoa(val + 2)
+	return f, val + 2, val + 2
 }
