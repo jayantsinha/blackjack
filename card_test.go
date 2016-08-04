5748bb2260556b485482cccaee6cbe2b391535a2
Jayant Sinha <jayant.sinha@hotmail.com>
2016-08-04 17:17:04 +0530

    Test an ACE of Hearts card

---
+++ card_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestNewCard(t *testing.T) {
+  card := Card{}
+  card = NewCard(SUIT_HEARTS, ACE)
+  aceCard := Card{
+    suit: SUIT_HEARTS,
+    value: 11,
+    alternateValue: 1,
+  }
+
+  if card.value != aceCard.value {
+    t.Errorf("Card value should be %d, found %d", aceCard.value, card.value)
+  }
+  if card.alternateValue != aceCard.alternateValue {
+    t.Errorf("Card value should be %d, found %d", aceCard.alternateValue, card.alternateValue)
+  }
+  if card.suit != aceCard.suit {
+    t.Errorf("Card value should be %d, found %d", aceCard.suit, card.suit)
+  }
+}
