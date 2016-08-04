2ca87f566ac6d98fcd2e06aa9304089fda0046ef
Jayant Sinha <jayant.sinha@hotmail.com>
2016-08-04 15:54:51 +0530

    all tests created

--- deck_test.go
+++ deck_test.go
@@ -14,3 +14,44 @@ func TestCreateNewDeck(t *testing.T) {
     t.Errorf("Expected deck size %d but got %d", expectedDeckSize, actualDeckSize)
   }
 }
+
+//Test shuffling
+func TestShuffle(t *testing.T) {
+  deckNormal := deck
+  deckShuffled := deck
+  deckNormal = CreateNewDeck()
+  deckShuffled = CreateNewDeck()
+  deckShuffled = deckShuffled.Shuffle()
+
+  if len(deckNormal) == len(deckShuffled) {
+    //Compare both decks
+    equality := 1
+    for i := 0; i<len(deckNormal); i++ {
+      if deckNormal[i] == deckShuffled[i] {
+        equality &= 1
+      } else {
+        equality &= 0
+      }
+    }
+    if equality == 1 {
+      t.Error("Expected the deck to be Shuffled!")
+    }
+  } else {
+    t.Errorf("Expected length of deck after shuffle to be %d but got %d", len(deckNormal), len(deckShuffled))
+  }
+
+}
+
+//Test for deck size after multiple draws
+func TestDraw(t *testing.T) {
+  deck := deck
+  deck = CreateNewDeck()
+  initialSizeOfDeck := len(deck)
+  _, deck = deck.Draw()
+  newSizeOfDeck := len(deck)
+  expectednewSize := initialSizeOfDeck - 1
+  if newSizeOfDeck != expectednewSize {
+    t.Errorf("Expected deck is %d, found %d", expectednewSize, newSizeOfDeck)
+  }
+
+}
