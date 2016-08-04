fea77e131891b38ff89c9923cf30a85002589430
Jayant Sinha <jayant.sinha@hotmail.com>
2016-08-05 03:05:09 +0530

    source formatting

--- deck.go
+++ deck.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-  "math/rand"
+	"math/rand"
 )
 
 type Deck []Card
 
 //Create a new Deck
 func CreateNewDeck() Deck {
-  deck := Deck{}
-  deck = createSuit(SUIT_HEARTS, deck)
-  deck = createSuit(SUIT_SPADES, deck)
-  deck = createSuit(SUIT_BLACKS, deck)
-  deck = createSuit(SUIT_DIAMONDS, deck)
-  return deck
+	deck := Deck{}
+	deck = createSuit(SUIT_HEARTS, deck)
+	deck = createSuit(SUIT_SPADES, deck)
+	deck = createSuit(SUIT_BLACKS, deck)
+	deck = createSuit(SUIT_DIAMONDS, deck)
+	return deck
 }
 
 //Add given suit type to the deck
 func createSuit(suit string, deck Deck) Deck {
-  for i := 0; i < CARDS_IN_SUIT; i++ {
-    deck = append(deck, NewCard(suit, i))
-  }
-  return deck
+	for i := 0; i < CARDS_IN_SUIT; i++ {
+		deck = append(deck, NewCard(suit, i))
+	}
+	return deck
 }
 
 //Knuth or Fisher-Yates shuffle
 func (deck Deck) Shuffle() Deck {
-  for i := range deck {
-    j := rand.Intn(i + 1)
-    deck[i], deck[j] = deck[j], deck[i]
-  }
-  return deck
+	for i := range deck {
+		j := rand.Intn(i + 1)
+		deck[i], deck[j] = deck[j], deck[i]
+	}
+	return deck
 }
 
 //Draw a card from the Deck
 func (deck Deck) Draw() (Card, Deck) {
-  return deck[0], deck[1:len(deck)]
+	return deck[0], deck[1:len(deck)]
 }
--- deck_test.go
+++ deck_test.go
@@ -6,61 +6,61 @@ var deck Deck
 
 //Check deck capacity after init
 func TestCreateNewDeck(t *testing.T) {
-  d := deck
-  d = CreateNewDeck()
-  actualDeckSize := len(d)
-  expectedDeckSize := 52
-  if actualDeckSize != expectedDeckSize {
-    t.Errorf("Expected deck size %d but got %d", expectedDeckSize, actualDeckSize)
-  }
+	d := deck
+	d = CreateNewDeck()
+	actualDeckSize := len(d)
+	expectedDeckSize := 52
+	if actualDeckSize != expectedDeckSize {
+		t.Errorf("Expected deck size %d but got %d", expectedDeckSize, actualDeckSize)
+	}
 }
 
 //Test shuffling
 func TestShuffle(t *testing.T) {
-  deckNormal := deck
-  deckShuffled := deck
-  deckNormal = CreateNewDeck()
-  deckShuffled = CreateNewDeck()
-  deckShuffled = deckShuffled.Shuffle()
+	deckNormal := deck
+	deckShuffled := deck
+	deckNormal = CreateNewDeck()
+	deckShuffled = CreateNewDeck()
+	deckShuffled = deckShuffled.Shuffle()
 
-  if len(deckNormal) == len(deckShuffled) {
-    //Compare both decks
-    equality := 1
-    for i := 0; i<len(deckNormal); i++ {
-      if deckNormal[i] == deckShuffled[i] {
-        equality &= 1
-      } else {
-        equality &= 0
-      }
-    }
-    if equality == 1 {
-      t.Error("Expected the deck to be Shuffled!")
-    }
-  } else {
-    t.Errorf("Expected length of deck after shuffle to be %d but got %d", len(deckNormal), len(deckShuffled))
-  }
+	if len(deckNormal) == len(deckShuffled) {
+		//Compare both decks
+		equality := 1
+		for i := 0; i < len(deckNormal); i++ {
+			if deckNormal[i] == deckShuffled[i] {
+				equality &= 1
+			} else {
+				equality &= 0
+			}
+		}
+		if equality == 1 {
+			t.Error("Expected the deck to be Shuffled!")
+		}
+	} else {
+		t.Errorf("Expected length of deck after shuffle to be %d but got %d", len(deckNormal), len(deckShuffled))
+	}
 
 }
 
- //TestDraw verifies the following:
- //1. Deck size should reduce after drawing a card
- //2. Drawn card should not exist in the deck
+//TestDraw verifies the following:
+//1. Deck size should reduce after drawing a card
+//2. Drawn card should not exist in the deck
 func TestDraw(t *testing.T) {
-  card := Card{}
-  deck := deck
-  deck = CreateNewDeck()
-  initialSizeOfDeck := len(deck)
-  card, deck = deck.Draw()
-  newSizeOfDeck := len(deck)
-  expectednewSize := initialSizeOfDeck - 1
-  if newSizeOfDeck != expectednewSize {
-    t.Errorf("Expected deck is %d, found %d", expectednewSize, newSizeOfDeck)
-  }
+	card := Card{}
+	deck := deck
+	deck = CreateNewDeck()
+	initialSizeOfDeck := len(deck)
+	card, deck = deck.Draw()
+	newSizeOfDeck := len(deck)
+	expectednewSize := initialSizeOfDeck - 1
+	if newSizeOfDeck != expectednewSize {
+		t.Errorf("Expected deck is %d, found %d", expectednewSize, newSizeOfDeck)
+	}
 
-  for r := range deck {
-    if deck[r] == card {
-      t.Errorf("Card %v should not exists in the deck", card)
-    }
-  }
+	for r := range deck {
+		if deck[r] == card {
+			t.Errorf("Card %v should not exists in the deck", card)
+		}
+	}
 
 }
