42ef843d8ae4d85d9590d51bf7d64c9fb387d0ea
Jayant Sinha <jayant.sinha@hotmail.com>
2016-08-08 05:44:48 +0530

    Fixed problem with for loop and gameplay

--- main.go
+++ main.go
@@ -50,28 +50,33 @@ func play() {
 	pHandVal := 0
 
 	for round.deck.HasMoreCards() {
-		if round.outcome == "ND" {
-			fmt.Println("Inside if")
+		if round.outcome == GAME_BUST || round.outcome == GAME_BLACKJACK || round.outcome == GAME_LOST || round.outcome == GAME_WON || round.outcome == "ND" {
+			round.dealer = Dealer{}
+			round.player = Human{}
 			for i := 0; i < 2; i++ {
 				card, round.deck = round.deck.Draw()
 				round.player.hand.AddCard(&card)
 				card, round.deck = round.deck.Draw()
-
 				round.dealer.hand.AddCard(&card)
 			}
-			fmt.Println(round.dealer)
+
 			round.num += 1
-			round.outcome = "ND"
+			round.outcome = ""
+			fmt.Printf("\n========================================================\n")
 			fmt.Printf("Dealer's Card: %v, [hidden card]\n", round.dealer.hand[0])
 			fmt.Printf("Your Cards: %v\n", round.player.hand)
 		}
 		pHandVal = player.hand.FavourableValue()
 		if pHandVal >= BUST {
+			round.outcome = GAME_BUST
 			fmt.Println(GAME_BUST)
+			fmt.Printf("Dealer's Hand: %v\n", round.dealer.hand)
 		} else if pHandVal == BLACKJACK {
+			round.outcome = GAME_BLACKJACK
 			fmt.Println(GAME_BLACKJACK)
+			fmt.Printf("Dealer's Hand: %v\n", round.dealer.hand)
 		} else {
-			for round.outcome != "ND" {
+			for round.outcome == "" {
 				fmt.Println("Enter your action: Hit or Stand or Surrender [H/S/X]: ")
 				reader.Scan()
 				text = reader.Text()
@@ -81,17 +86,27 @@ func play() {
 					round.player.DealCard(card)
 					fmt.Println("Your card: ", card)
 					if round.player.hand.FavourableValue() == BLACKJACK {
-						round.outcome = GAME_BLACKJACK
-						fmt.Println(GAME_BLACKJACK)
+						if round.dealer.hand.FavourableValue() == BLACKJACK {
+							round.outcome = GAME_DRAW
+							fmt.Printf("Dealer's Hand: %v\n", round.dealer.hand)
+							fmt.Println(GAME_DRAW)
+						} else {
+							round.outcome = GAME_BLACKJACK
+							fmt.Printf("Dealer's Hand: %v\n", round.dealer.hand)
+							fmt.Println(GAME_BLACKJACK)
+						}
 					} else if round.player.hand.FavourableValue() >= BUST {
 						round.outcome = GAME_BUST
+						fmt.Printf("Dealer's Hand: %v\n", round.dealer.hand)
 						fmt.Println(GAME_BUST)
 					}
 				case "S", "s":
 					round = round.DealersTurn()
+					fmt.Printf("Dealer's Hand: %v\n", round.dealer.hand)
 					fmt.Println(round.outcome)
 				case "X", "x":
 					round.outcome = GAME_LOST
+					fmt.Printf("Dealer's Hand: %v\n", round.dealer.hand)
 					fmt.Println(GAME_LOST)
 				}
 			}
